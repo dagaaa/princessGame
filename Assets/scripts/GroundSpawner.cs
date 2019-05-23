98e74cd6f3e127a926f004b707e2ee70cacde3a8
dagaaa <dagmaragoj@gmail.com>
2019-05-23 19:57:45 +0200

    initial commit

---
+++ Assets/scripts/GroundSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int maxGrounds = 20;
+    public GameObject ground;
+    public float width = 10.24f;
+
+    private Vector2 originPosition;
+
+
+    void Start () {
+        
+        originPosition = transform.position;
+        Spawn ();
+
+    }
+
+    void Spawn()
+    {
+        for (int i = 0; i < maxGrounds; i++)
+        {
+            Vector2 nextPosition =  new Vector2 (originPosition.x+width, originPosition.y );
+            Instantiate(ground, nextPosition, Quaternion.identity);
+            originPosition = nextPosition;
+        }
+    }
+}
---
+++ Assets/scripts/PlatformSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PlatformSpawner : MonoBehaviour
+{
+    public int maxPlatforms = 20;
+    public GameObject platform;
+    public float horizontalMin = 1.5f;
+    public float horizontalMax = 3.5f;
+    public float verticalMin = -3f;
+    public float verticalMax = 2f;
+
+
+    private Vector2 originPosition;
+
+
+    void Start () {
+
+        originPosition = transform.position;
+        Spawn ();
+
+    }
+
+    void Spawn()
+    {
+        for (int i = 0; i < maxPlatforms; i++)
+        {
+            float newHorizontal = originPosition.x+Random.Range(horizontalMin, horizontalMax);
+            float newVertical = Math.Max(0.5f, originPosition.y + Random.Range(verticalMin, verticalMax));
+            Vector2 randomPosition = new Vector2 (newHorizontal, newVertical);
+            Instantiate(platform, randomPosition, Quaternion.identity);
+            originPosition = randomPosition;
+        }
+    }
+
+}
