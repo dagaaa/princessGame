3712963a34588e0da86ecaf2778af270def02338
Paweł Matejko <matejko.pawel@gmail.com>
2019-05-29 10:22:16 +0200

    added dynamic waypoints for enemies

--- Assets/scripts/FollowThePath.cs
+++ Assets/scripts/FollowThePath.cs
@@ -1,31 +1,37 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
 using UnityEngine;
-using UnityEngine.UIElements;
+using Random = System.Random;
 
 public class FollowThePath : MonoBehaviour
 {
-    [SerializeField] private Transform[] waypoints;
+    private GameObject[] dynamicWayPoints;
 
     private float TOLERANCE = 0.1f;
 
     [SerializeField] private float moveSpeed = 2f;
     private int waypointIndex = 0;
+    private Random _random = new Random();
+    private bool facingRight = true;
 
 
     private void Start()
     {
-        foreach (var waypoint in waypoints)
+        dynamicWayPoints = GameObject.FindGameObjectsWithTag("WayPoint");
+
+        foreach (var waypoint in dynamicWayPoints)
         {
             var transformPosition = transform.position;
-            waypoint.position = new Vector3(
-                Math.Abs(waypoint.position.x + transformPosition.x),
+            waypoint.transform.position = new Vector3(
+                Math.Abs(waypoint.transform.position.x + transformPosition.x),
                 transformPosition.y,
                 transformPosition.z);
         }
 
-        for (int i = 0; i < waypoints.Length; i++)
+        for (int i = 0; i < dynamicWayPoints.Length; i++)
         {
-            if (waypoints[i].position == transform.position)
+            if (dynamicWayPoints[i].transform.position == transform.position)
             {
                 waypointIndex = i;
                 return;
@@ -35,20 +41,81 @@ public class FollowThePath : MonoBehaviour
 
     private void Update()
     {
-        Move();
+        if (dynamicWayPoints.Length < 3)
+        {
+            dynamicWayPoints = GameObject.FindGameObjectsWithTag("WayPoint");
+        }
+        else
+        {
+            Move();
+        }
     }
-
 
     private void Move()
     {
         transform.position = Vector2.MoveTowards(
             transform.position,
-            waypoints[waypointIndex].transform.position,
+            dynamicWayPoints[waypointIndex].transform.position,
             moveSpeed);
 
-        if (Math.Abs(transform.position.x - waypoints[waypointIndex].transform.position.x) < TOLERANCE)
+        if (Math.Abs(transform.position.x - dynamicWayPoints[waypointIndex].transform.position.x) < TOLERANCE)
         {
-            waypointIndex = (waypointIndex + 1) % waypoints.Length;
+            if (_random.Next(0, 2) == 0)
+            {
+                SetNextRightIndex(waypointIndex + 1, waypointIndex - 1);
+            }
+            else
+            {
+                SetNextLeftIndex(waypointIndex - 1, waypointIndex + 1);
+            }
         }
+    }
+
+    private void SetNextRightIndex(int nextIndex, int fallbackIndex)
+    {
+        if (nextIndex <= dynamicWayPoints.Length - 1)
+        {
+            waypointIndex = nextIndex;
+            if (!facingRight)
+            {
+                Flip();
+            }
+        }
+        else
+        {
+            waypointIndex = fallbackIndex;
+            if (facingRight)
+            {
+                Flip();
+            }
+        }
+    }
+
+    private void SetNextLeftIndex(int nextIndex, int fallbackIndex)
+    {
+        if (nextIndex >= 0)
+        {
+            waypointIndex = nextIndex;
+            if (facingRight)
+            {
+                Flip();
+            }
+        }
+        else
+        {
+            waypointIndex = fallbackIndex;
+            if (!facingRight)
+            {
+                Flip();
+            }
+        }
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
--- Assets/scripts/GroundSpawner.cs
+++ Assets/scripts/GroundSpawner.cs
@@ -7,24 +7,31 @@ public class GroundSpawner : MonoBehaviour
     // Start is called before the first frame update
     public int maxGrounds = 20;
     public GameObject ground;
+    public GameObject wayPoint = null;
     public float width = 10.24f;
 
     private Vector2 originPosition;
 
 
-    void Start () {
-        
+    void Start()
+    {
         originPosition = transform.position;
-        Spawn ();
-
+        Spawn();
     }
 
     void Spawn()
     {
         for (int i = 0; i < maxGrounds; i++)
         {
-            Vector2 nextPosition =  new Vector2 (originPosition.x+width, originPosition.y );
+            Vector2 nextPosition = new Vector2(originPosition.x + width, originPosition.y);
             Instantiate(ground, nextPosition, Quaternion.identity);
+
+            if (wayPoint != null)
+            {
+                Vector2 nextWayPointPosition = new Vector2(originPosition.x + (1.5f * width), originPosition.y + 1f);
+                Instantiate(wayPoint, nextWayPointPosition, Quaternion.identity);
+            }
+
             originPosition = nextPosition;
         }
     }
