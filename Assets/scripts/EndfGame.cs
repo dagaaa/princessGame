d4a17e7ca11c710adc5740e3b6e4abb8654a262f
Paweł Matejko <matejko.pawel@gmail.com>
2019-05-29 11:00:26 +0200

    Added end of game

--- Assets/scripts/EndfGame.cs
+++ Assets/scripts/EndfGame.cs
@@ -7,65 +7,60 @@ public class EndfGame : MonoBehaviour
 {
     private int allCoins = 0;
     public TextMeshProUGUI endOfGameText;
+
     private int pickupedCoins = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         endOfGameText.text = "";
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            
-            int coinsToWin =(allCoins * 80) / 100;
-            if (pickupedCoins  >= coinsToWin)
+            int coinsToWin = (allCoins * 80) / 100;
+            if (pickupedCoins >= coinsToWin)
             {
-                
                 endOfGameText.text = "You won";
+                Destroy(other.gameObject.GetComponent<Controller>());
+                Destroy(other.gameObject.GetComponent<PickupCoin>());
+                Destroy(Camera.main.GetComponent<CameraFollower>());
                 ActivateMe();
             }
             else
             {
                 endOfGameText.text = "You need to pickup " + coinsToWin.ToString();
                 DeactivateMe();
-               
             }
-            
-
         }
     }
-    
-    public void ActivateMe (){
-        endOfGameText.gameObject.SetActive (true);
+
+    public void ActivateMe()
+    {
+        endOfGameText.gameObject.SetActive(true);
+        Time.timeScale = 0f;
     }
- 
-    public void DeactivateMe (){
+
+    public void DeactivateMe()
+    {
         StartCoroutine(RemoveAfterSeconds(3));
     }
- 
-    IEnumerator RemoveAfterSeconds (int seconds){
+
+    IEnumerator RemoveAfterSeconds(int seconds)
+    {
         yield return new WaitForSeconds(seconds);
         endOfGameText.gameObject.SetActive(false);
     }
 
-   public void UpdateCoins( int pickuped)
+    public void UpdateCoins(int pickuped)
     {
-        
         this.pickupedCoins = pickuped;
-
     }
-   public void setCoins( int all)
-   {
-        
-       this.allCoins += all;
 
-   }
+    public void setCoins(int all)
+    {
+        this.allCoins += all;
+    }
 }
--- Assets/scripts/EnemyInteractions.cs
+++ Assets/scripts/EnemyInteractions.cs
@@ -1,20 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
 
 public class EnemyInteractions : MonoBehaviour
 {
     public TextMeshProUGUI livesText;
     public TextMeshProUGUI ghostsText;
+    public TextMeshProUGUI endOfGameText;
 
     public int lives = 3;
     private int _killedGhosts = 0;
 
     void Start()
     {
-        setLivesText();
-        setGhostsText();
+        SetLivesText();
+        SetGhostsText();
     }
     
     void OnTriggerEnter2D(Collider2D other)
@@ -23,30 +22,41 @@ public class EnemyInteractions : MonoBehaviour
         {
             Destroy(other.transform.parent.gameObject);
             _killedGhosts++;
-            setGhostsText();
+            SetGhostsText();
         }
         else if (other.gameObject.CompareTag("EnemySide"))
         {
             if (--lives <= 0)
             {
-                Destroy(this.gameObject);
+                ActivateMe();
             }
             else
             {
                 var position = transform.position;
                 transform.position = new Vector3(position.x, position.y + 10f, position.z);
             }
-            setLivesText();
+            SetLivesText();
         }
     }
 
-    private void setLivesText()
+    private void SetLivesText()
     {
         livesText.text = "Lives:" + lives;
     }
 
-    private void setGhostsText()
+    private void SetGhostsText()
     {
         ghostsText.text = "Killed:" + _killedGhosts;
+    }
+    
+    public void ActivateMe ()
+    {
+        endOfGameText.text = "YOU LOST!";
+        endOfGameText.gameObject.SetActive (true);
+        Destroy(gameObject.GetComponent<Controller>());
+        Destroy(gameObject.GetComponent<PickupCoin>());
+        Destroy(Camera.main.GetComponent<CameraFollower>());
+        Destroy(gameObject);
+        Time.timeScale = 0f;
     }
 }
